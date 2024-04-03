824e97cf8f5163023689abf48043f1626e472b57
tinsae <tinsae.yb@gmail.com>
2024-04-02 20:35:02 -0500

    Create GSON adapter for PensionPlan and LocalDate

--- src/main/java/org/example/PensionPlanAdapter.java
+++ src/main/java/org/example/PensionPlanAdapter.java
@@ -1,2 +1,38 @@
-package org.example;public class PensionPlanAdapter {
+package org.example;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class PensionPlanAdapter implements JsonSerializer<PensionPlan>, JsonDeserializer<PensionPlan> {
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    public JsonElement serialize(PensionPlan src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("planReferenceNumber", src.getPlanReferenceNumber());
+        jsonObject.addProperty("enrollmentDate", formatter.format(src.getEnrollmentDate()));
+        jsonObject.addProperty("monthlyContribution", src.getMonthlyContribution());
+        return jsonObject;
+    }
+
+    @Override
+    public PensionPlan deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+            throws JsonParseException {
+
+        JsonObject jsonObject = json.getAsJsonObject();
+        String planReferenceNumber = jsonObject.get("planReferenceNumber").getAsString();
+        LocalDate enrollmentDate = LocalDate.parse(jsonObject.get("enrollmentDate").getAsString(), formatter);
+        double monthlyContribution = jsonObject.get("monthlyContribution").getAsDouble();
+
+
+        PensionPlan pensionPlan = new PensionPlan();
+        pensionPlan.setPlanReferenceNumber(planReferenceNumber);
+        pensionPlan.setEnrollmentDate(enrollmentDate);
+        pensionPlan.setMonthlyContribution(monthlyContribution);
+        return pensionPlan;
+    }
 }
