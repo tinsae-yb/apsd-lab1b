a0b4d2012da214ee8abcf979826107a8e63c6b9d
tinsae <tinsae.yb@gmail.com>
2024-04-02 20:34:21 -0500

    Create Empluee and PensionPlan Entity

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,2 +1,33 @@
-package org.example;public class Employee {
-}
+package org.example;
+
+
+    import lombok.Data;
+
+    import java.time.LocalDate;
+
+    @Data
+    public class Employee implements Comparable<Employee> {
+        private Long employeeId;
+        private String firstName;
+        private String lastName;
+        private LocalDate employmentDate;
+        private Double yearlySalary;
+        private PensionPlan pensionPlan;
+
+
+
+        @Override
+        public int compareTo(Employee other) {
+
+            int lastNameCompare = this.lastName.compareTo(other.lastName);
+            if (lastNameCompare != 0) {
+                return lastNameCompare;
+            } else {
+                return Double.compare(other.yearlySalary, this.yearlySalary);
+            }
+        }
+
+
+    }
+
+
